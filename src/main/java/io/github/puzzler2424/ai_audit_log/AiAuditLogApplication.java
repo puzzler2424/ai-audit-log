d113d9c8c816fb8baa0fad4116a0cfa111d8e38d
puzzler2424 <puzzler2424@gmail.com>
2026-01-06 15:58:41 +0900

    feat: Mysql 연동 및 로그용 엔티티 추가

--- src/main/java/io/github/puzzler2424/ai_audit_log/AiAuditLogApplication.java
+++ src/main/java/io/github/puzzler2424/ai_audit_log/AiAuditLogApplication.java
@@ -2,9 +2,8 @@ package io.github.puzzler2424.ai_audit_log;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class AiAuditLogApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/io/github/puzzler2424/ai_audit_log/Entity/LogEntry.java
@@ -0,0 +1,31 @@
+package io.github.puzzler2424.ai_audit_log.Entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter
+public class LogEntry {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String userId;      // 누가?
+
+    @Column(columnDefinition = "TEXT")
+    private String prompt;      // 질문
+
+    @Column(columnDefinition = "TEXT")
+    private String answer;      // AI 답변
+
+    private String hashValue;   // 조작 방지용 해시
+
+    private LocalDateTime createdAt; // 언제?
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+    }
+}
