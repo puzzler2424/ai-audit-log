016dc6769cb835d866afd5a91177df99e300f742
puzzler2424 <puzzler2424@gmail.com>
2026-01-06 15:00:27 +0900

    feat: 초기 프로젝트 세팅
    # Conflicts:
    #	.gitignore

---
+++ src/main/java/io/github/puzzler2424/ai_audit_log/AiAuditLogApplication.java
@@ -0,0 +1,13 @@
+package io.github.puzzler2424.ai_audit_log;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiAuditLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiAuditLogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/puzzler2424/ai_audit_log/AiAuditLogApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.puzzler2424.ai_audit_log;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiAuditLogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
